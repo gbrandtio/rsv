8b22484945663179a70a54882beb377362faea3b
Giannis Brandt <john-brandt@windowslive.com>
2019-10-28 01:50:50 +0000

    This adapter is attached to NestedFragmentFeed, where it shows the reservations schedulded for today and the upcoming day.

---
+++ src/main/java/adapters/AdapterFeed.java
@@ -0,0 +1,125 @@
+package com.example.root.rsv.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.root.rsv.models.RSV;
+import com.example.root.rsv.R;
+import com.example.root.rsv.fragments.NestedFragmentRSVs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class AdapterFeed extends RecyclerView.Adapter<AdapterFeed.ViewHolder> {
+
+    private List<RSV> mData;
+    private List<RSV> tempData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    public AdapterFeed(Context context, List<RSV> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+        tempData = new ArrayList<>();
+        tempData.addAll(mData);
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.item_lists_rsvs_feed, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String id = mData.get(position).getId();
+        String car_name = mData.get(position).getCar_id();
+        String date = mData.get(position).getDay_start();
+        date = date + " / " + mData.get(position).getDay_end();
+        String client = mData.get(position).getClient_id();
+
+        holder.tvRsvId.setText(id);
+        holder.tvCar.setText(car_name);
+        holder.tvClient.setText(client);
+        holder.tvDate.setText(date);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvRsvId,tvCar,tvDate,tvClient;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            tvRsvId = itemView.findViewById(R.id.label_rsv_id);
+            tvCar  = itemView.findViewById(R.id.label_rsv_car);
+            tvDate = itemView.findViewById(R.id.label_rsv_date);
+            tvClient = itemView.findViewById(R.id.label_rsv_client);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    public String getItem(int id) {
+        return mData.get(id).getId();
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    // Filter Class
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        NestedFragmentRSVs.list.clear();
+        if (charText.length() == 0) {
+            NestedFragmentRSVs.list.addAll(tempData);
+        } else {
+            for (RSV rsv : tempData) {
+                if(rsv.getId().toLowerCase(Locale.ENGLISH).contains(charText)){
+                    NestedFragmentRSVs.list.add(rsv);
+                }
+                else if (rsv.getCar_id().toLowerCase(Locale.ENGLISH).contains(charText)) {
+                    NestedFragmentRSVs.list.add(rsv);
+                }
+                else if(rsv.getClient_id().toLowerCase(Locale.ENGLISH).contains(charText)){
+                    NestedFragmentRSVs.list.add(rsv);
+                }
+                else if(rsv.getDay_start().toLowerCase(Locale.ENGLISH).contains(charText)){
+                    NestedFragmentRSVs.list.add(rsv);
+                }
+                else if(rsv.getDay_end().toLowerCase(Locale.ENGLISH).contains(charText)){
+                    NestedFragmentRSVs.list.add(rsv);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+}
+
