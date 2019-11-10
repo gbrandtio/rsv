ce90371a27a920aab226968146cf09e9aa5b91e6
Giannis Brandt <john-brandt@windowslive.com>
2019-11-10 18:40:48 +0000

    Updated AdapterCars w/ functionality to receive statistics for each individual car and open a customized popup window to show them. (statistics concerning reservations and clients of the specified car).

--- src/main/java/adapters/AdapterCars.java
+++ src/main/java/adapters/AdapterCars.java
@@ -1,9 +1,13 @@
 package com.example.root.rsv.adapters;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +20,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.root.rsv.MainActivity;
@@ -24,8 +29,14 @@ import com.example.root.rsv.models.Car;
 import com.example.root.rsv.models.RSV;
 import com.example.root.rsv.R;
 import com.example.root.rsv.fragments.NestedFragmentRSVs;
+import com.example.root.rsv.models.SingleCarStats;
 import com.example.root.rsv.views.ActivityCategoryCars;
 import com.example.root.rsv.views.ActivityEditCar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -88,6 +99,32 @@ public class AdapterCars extends RecyclerView.Adapter<AdapterCars.ViewHolder> {
                 deleteCar(id);
             }
         });
+
+        holder.btnStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog statsDialog = new Dialog(context);
+                statsDialog.setContentView(R.layout.car_stats_popup);
+                holder.tvStatsName = (TextView) statsDialog.findViewById(R.id.car_stats_name);
+                holder.tvStatsCategory = (TextView) statsDialog.findViewById(R.id.car_stats_category);
+                holder.tvYearlyRsvs = (TextView) statsDialog.findViewById(R.id.car_stats_total_rsvs_year);
+                holder.tvMonthlyRsvs = (TextView) statsDialog.findViewById(R.id.car_stats_total_rsvs_month);
+                holder.tvPreviousService = (TextView) statsDialog.findViewById(R.id.car_stats_previous_service);
+                holder.tvNextService = (TextView) statsDialog.findViewById(R.id.car_stats_next_service);
+                holder.tvNationalityPercentage = (TextView) statsDialog.findViewById(R.id.nationality_percentage);
+                holder.tvCloseDialog = (TextView) statsDialog.findViewById(R.id.car_stats_close_dialog);
+                holder.tvCloseDialog.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        statsDialog.dismiss();
+                    }
+                });
+                statsDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                statsDialog.show();
+
+                getStats(id,holder);
+            }
+        });
     }
 
     // total number of rows
@@ -100,7 +137,8 @@ public class AdapterCars extends RecyclerView.Adapter<AdapterCars.ViewHolder> {
     // stores and recycles views as they are scrolled off screen
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView tvCarName,tvKms,tvLicensePlate,tvCategory;
-        ImageButton btnEditCar,btnDeleteCar;
+        TextView tvStatsName,tvStatsCategory,tvYearlyRsvs,tvMonthlyRsvs,tvPreviousService,tvNextService,tvNationalityPercentage,tvCloseDialog;
+        ImageButton btnEditCar,btnDeleteCar,btnStats,btnService;
 
         ViewHolder(View itemView) {
             super(itemView);
@@ -108,8 +146,11 @@ public class AdapterCars extends RecyclerView.Adapter<AdapterCars.ViewHolder> {
             tvCategory = (TextView) itemView.findViewById(R.id.item_list_car_category);
             tvKms = (TextView) itemView.findViewById(R.id.item_list_car_kms);
             tvLicensePlate = (TextView) itemView.findViewById(R.id.item_list_car_license_plate);
+            tvCloseDialog = (TextView) itemView.findViewById(R.id.car_stats_close_dialog);
             btnEditCar = (ImageButton) itemView.findViewById(R.id.btn_item_list_edit_car);
             btnDeleteCar = (ImageButton) itemView.findViewById(R.id.btn_item_list_delete_car);
+            btnStats = (ImageButton) itemView.findViewById(R.id.btn_item_list_car_stats);
+            btnService = (ImageButton) itemView.findViewById(R.id.btn_item_list_car_alarm);
 
             itemView.setOnClickListener(this);
         }
@@ -135,6 +176,49 @@ public class AdapterCars extends RecyclerView.Adapter<AdapterCars.ViewHolder> {
         void onItemClick(View view, int position);
     }
 
+    public void getStats(String id,ViewHolder holder){
+        String url = context.getResources().getString(R.string.base_url) +
+                     context.getResources().getString(R.string.singleCarStats) + id;
+        System.out.println("EXECUTED: " + id);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        SingleCarStats stats = new SingleCarStats(
+                                jsonObject.getString("id"),jsonObject.getString("name"),
+                                jsonObject.getString("category"),jsonObject.getString("kms"),
+                                jsonObject.getString("license_plate"),jsonObject.getString("total_rsvs"),
+                                jsonObject.getString("image"),jsonObject.getString("monthly_rsvs"),
+                                jsonObject.getString("yearly_rsvs"),jsonObject.getString("nationality_percentage"),
+                                jsonObject.getString("nam_nationality"),jsonObject.getString("previous_service_date"),
+                                jsonObject.getString("upcoming_service_date")
+                        );
+                        holder.tvStatsName.setText(stats.getName()+"\n"+stats.getLicense_plate());
+                        holder.tvStatsCategory.setText("Category: " + stats.getCategory());
+                        holder.tvMonthlyRsvs.setText(stats.getMonthly_rsvs());
+                        holder.tvYearlyRsvs.setText(stats.getYearly_rsvs());
+                        holder.tvPreviousService.setText(stats.getPrevious_service_date());
+                        holder.tvNextService.setText(stats.getUpcoming_service_date());
+                        holder.tvNationalityPercentage.setText(stats.getNationality_percentage()+"\n"+stats.getNam_nationality());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("SiCarSt", error.toString());
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(jsonArrayRequest);
+    }
+
+
     public void editCar(String car_name,String car_category,String car_kms,String car_license_plate,
                         String car_total_rsvs){
         Intent intent = new Intent(context, ActivityEditCar.class);
@@ -145,6 +229,7 @@ public class AdapterCars extends RecyclerView.Adapter<AdapterCars.ViewHolder> {
         intent.putExtra("CAR_TOTAL_RSVS",car_total_rsvs);
         context.startActivity(intent);
     }
+
     //This function is used to make a post request to the API to delete the given car.
     public void deleteCar(String car_id){
         String url = context.getResources().getString(R.string.base_url) + context.getResources().getString(R.string.deletecar);
